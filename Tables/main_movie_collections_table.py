eb9d154a15849a92892ec84f154ff5698cb3bdf1
mason-mcgregor <mason.mcgregor@npbhs.school.nz>
2024-08-15 12:02:08 +1200

    15/08/2024

--- Tables/main_movie_collections_table.py
+++ Tables/main_movie_collections_table.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('film-collection.db')
 
 conn.execute('''
-    CREATE TABLE genre_table(
+    CREATE TABLE movie_collection_table(
         id INTEGER PRIMARY KEY,
         key TEXT NOT NULL UNIQUE,
         genre TEXT NOT NULL
