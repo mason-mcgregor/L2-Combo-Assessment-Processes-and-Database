727609226c7859b23463555feb471a6a509d305d
mason-mcgregor <mason.mcgregor@npbhs.school.nz>
2024-08-22 10:35:39 +1200

    22/08/2024
    Messed around with my database

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ MENU_TITLE = "Main menu"
 conn = sqlite3.connect('film-collection.db')
 cursor = conn.cursor()
 
-output=""
+
+output = ""
+items =""
 
 menu_action = easygui.buttonbox("Welcome to your Movie Catalog!", title=MENU_TITLE, choices=MENU_BUTTONS)
 
 if menu_action == 'View':
-    for row in cursor.execute("SELECT * FROM movie_collection_table ORDER BY ID ASC"):
-        id, movie_name, movie_release_date, movie_rating, movie_run_time, movie_genre = row
-        output += f"{row} \n"
+    for row in cursor.execute("SELECT movie_collection_table.id, movie_name, movie_release_date, movie_rating, movie_run_time, genre FROM movie_collection_table INNER JOIN genre_table ON movie_collection_table.movie_genre=genre_table.ID ORDER BY movie_collection_table.ID ASC"):
+        id, movie_name, movie_release_date, movie_rating, movie_run_time, genre = row
+        for item in row:
+            items += f"{item} "
+        output += f"{items} \n"
+        items = ""
+        
     easygui.msgbox(output)
     
 
