ce6a0ad3fff6c33ff200d4211e625b10ecc8fa32
mason-mcgregor <mason.mcgregor@npbhs.school.nz>
2024-09-12 13:04:04 +1200

    Version 15 (12/09/24)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sqlite3
 import easygui
 from tabulate import tabulate
-from custom_choice_box.py import customchoicebox
 
 MENU_BUTTONS = ['Add', 'View', 'Search', 'Delete', 'Exit']
 MENU_TITLE = "Main menu"
@@ -20,7 +19,7 @@ if menu_action == 'View':
     filter = "movie_collection_table.ID"
     while not sort == "exit":
         output = cursor.execute(f"SELECT movie_collection_table.id, movie_name, movie_release_date, movie_rating, movie_run_time, genre FROM movie_collection_table INNER JOIN genre_table ON movie_collection_table.movie_genre=genre_table.ID ORDER BY {filter} {sort}")
-        veiw_sort = easygui.customchoicebox(tabulate(output, headers=VIEW_HEADERS), choices=FILTER_CHOICES)
+        veiw_sort = easygui.choicebox(tabulate(output, headers=VIEW_HEADERS), choices=FILTER_CHOICES)
         if veiw_sort == "Descending":
             sort = "DESC"
         elif veiw_sort == "Ascending":
